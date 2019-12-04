39db959751fe0b402245f1884e3ac63053fe42f8
monaakbarpoor <45979598+Mnaakbarpoor@users.noreply.github.com>
2019-12-04 17:19:41 +0200

    Javascript-week2

---
+++ javascript-week2/homework2.js
@@ -0,0 +1,176 @@
+// // 1
+
+// console.log('Hello World');
+// console.log('Halo dunia');
+// console.log('Ciao mondo');
+// console.log('Hola mundo');
+
+// // 2
+
+// console.log("I'm awseom");
+
+// // 3
+// // 3.1
+// let x;
+
+// // 3.2
+// console.log('the value of my variable x will be : whatEverYouThink');
+
+// // 3.3
+// console.log(x);
+
+// // 3.4
+// x = 12;
+
+// // 3.5
+// console.log('the value of my variable y will be : whatEverYouThink');
+
+// // 3.6
+// console.log(x);
+
+// // 4
+// let y = 'this is y';
+
+// // 4.1
+// console.log('the value of my variable y will be : whatEverYouThink');
+
+// // 4.2
+// console.log(y)
+
+// // 4.3
+// y= "this y is changed";
+
+// // 4.4
+// console.log('the value of my variable y will be : whatEverYouThink');
+
+// // 4.5
+// console.log(y);
+
+// // 5
+
+// // 5.1
+// let z = 7.25;
+
+// // 5.2
+// console.log(z);
+
+// // 5.3
+// let a = Math.round(z)
+
+// // 5.4
+// console.log(a)
+
+// // 5.5
+// let highestValue = a > z ? a : z;
+
+// // 5.6
+// console.log(highestValue);
+
+// // 6
+
+// // 6.1
+// let plurality = [];
+
+// // 6.2
+// console.log('the value of my variable y will be : whatEverYouThink');
+
+// // 6.3
+// console.log(plurality);
+
+// // 6.4
+// let favAnimal = ['panda' , 'horse' , 'rabbite'];
+
+// // 6.5
+// console.log(favAnimal);
+
+// // 6.6
+// favAnimal.push('baby pigs');
+
+// // 6.7
+// console.log(favAnimal)
+
+// // 7
+
+// // 7.1
+// let myString = "this is a test"
+
+// // 7.2
+// console.log(myString);
+
+// // 7.3 & 7.4
+// console.log(myString.length);
+
+// // 8
+
+// // 8.1
+// let myNumber = 12;
+// let myText = 'this is string';
+// let myObject = {};
+// let myArray = [];
+
+// // 8.2
+
+// console.log(myNumber);
+// console.log(myText);
+// console.log(myObject);
+// console.log(myArray);
+
+// // 8.3
+// console.log('The value of my variable myNumber is: ' + myNumber);
+// console.log('The value of my variable myText is: ' + myText);
+// console.log('The value of my variable myObject is: ' + myObject);
+// console.log('The value of my variable myArray is: ' + myArray);
+
+// // 8.4
+// console.log('what you think the type of'+ myNumber +'is');
+// console.log('what you think the type of'+ myText +'is');
+// console.log('what you think the type of'+ myObject +'is');
+// console.log('what you think the type of'+ myArray +'is');
+
+// // 8.5
+// console.log(typeof myNumber);
+// console.log(typeof myText);
+// console.log(typeof myObject);
+// console.log(typeof myArray);
+
+// // 8.6
+// if(typeof myNumber === typeof myText){
+//     console.log('SAME TYPE');
+// }else if(typeof myNumber === typeof myObject){
+//     console.log('SAME TYPE')
+// }else if(typeof myNumber === typeof myArray){
+//     console.log('SAME TYPE')
+// }else if(typeof myObject === typeof myArray){
+//     console.log('SAME TYPE')
+// }else{
+//     console.log('not SAME TYPE')
+// }
+
+// // 9
+// let x = 7
+// console.log(x%3)
+
+// // 9.1
+// console.log(x % 2);
+// console.log(x % 4);
+// console.log(x % 5);
+
+// // 10
+
+// // 10.1
+// let typeArray = [12 , 'hello'];
+// console.log(typeArray);
+
+// // 10.2
+// if(6/0 === 10 /0){
+//     console.log('they are equal')
+// }else{
+//     console.log('they are not equal')
+// }
+
+
+
+
+
+
+
